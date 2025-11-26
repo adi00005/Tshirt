63185c826bd6cc23b9517a555f54af7f6d17b34b
Aditya <p4u.intern@gmail.com>
2025-11-26 13:42:41 +0530

    5 commit

--- Tshirt/Tshirt/frontend/src/services/api.js
+++ Tshirt/Tshirt/frontend/src/services/api.js
@@ -5,6 +5,10 @@ import { getAuthToken } from '../utils/auth';
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // To this:
+// Change this:
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+// To this:
 const API_URL = 'https://tshirt-backend-nm5u.onrender.com/api';
 
 const api = axios.create({
